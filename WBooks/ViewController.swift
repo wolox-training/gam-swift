d15eb6c5e8f799460ce05dd58c0c839de9efd96d
Gastón Máspero <gaston.maspero@wolox.com.ar>
2019-05-20 16:43:25 -0300

    Added print statement associated to sign in button

--- WBooks/ViewController.swift
+++ WBooks/ViewController.swift
@@ -13,10 +13,10 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Actions
+   
+    @IBAction func signInGoogle(_ sender: UIButton) {
+        print("Hello Wolox!")
     }
-
 }
