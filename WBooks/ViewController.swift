10a19f980a8498b66125541a3009387cfbabe0ab
Gastón Máspero <gaston.maspero@wolox.com.ar>
2019-05-20 12:24:36 -0300

    Fixed directory layout

---
+++ WBooks/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WBooks
+//
+//  Created by Guido Marucci Blas on 4/3/16.
+//  Copyright © 2016 Wolox. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
