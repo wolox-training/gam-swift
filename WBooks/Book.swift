d854d8bae3648f108f15712178fb9efcb8009362
Gastón Máspero <gaston.maspero@wolox.com.ar>
2019-05-22 11:07:23 -0300

    Dummy table view now works

---
+++ WBooks/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  WBooks
+//
+//  Created by Gaston Maspero on 22/05/2019.
+//  Copyright © 2019 Wolox. All rights reserved.
+//
+
+import Foundation
+
+class Book{
+    var image: UIImage
+    var title: String
+    var author: String
+}
--- WBooks/MainMenu.swift
+++ WBooks/MainMenu.swift
@@ -2,29 +2,53 @@
 //  MainMenu.swift
 //  WBooks
 //
-//  Created by Gaston Maspero on 20/05/2019.
+//  Created by Gaston Maspero on 22/05/2019.
 //  Copyright © 2019 Wolox. All rights reserved.
 //
 
 import UIKit
 
-class MainMenu: UIViewController {
+class MainMenu: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var books: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        books = createArray()
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let text = books[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myCustomCell") as? CustomCell else {
+            print("Error")
+            return CustomCell()
+        }
+        cell.setText(text: text)
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func createArray() ->[String]{
+        var books: [String] = []
+        books.append("Books 1")
+        books.append("Books 2")
+        books.append("Books 3")
+        books.append("Books 4")
+        books.append("Books 5")
+        books.append("Books 6")
+        books.append("Books 7")
+        books.append("Books 8")
+        
+        return books
+        
+    }
 
 }
